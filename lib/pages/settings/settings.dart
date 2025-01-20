910eb3ad13d42056e0f81df5783ac3bdef2d9549
archp4 <archpatel4@gmail.com>
2025-01-20 13:35:20 -0500

    Added Tour on Setting

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:features_tour/features_tour.dart';
 import 'package:flutter/material.dart';
 import 'package:pwd_mng/helpers/password_helper.dart';
 import 'package:pwd_mng/helpers/settings.dart';
@@ -17,6 +18,8 @@ class _SettingsState extends State<Settings> {
   var isDark = true;
   var isAuth = true;
 
+  final tourController = FeaturesTourController("Settings");
+
   @override
   Widget build(BuildContext context) {
     isAuth = context.watch<AppSettings>().isAuth;
@@ -31,57 +34,77 @@ class _SettingsState extends State<Settings> {
       ),
       body: Column(
         children: [
-          ListTile(
-            leading: const Icon(Icons.brightness_4),
-            title: const Text("Dark Mode"),
-            trailing: Switch(
-              value: isDark,
-              onChanged: (val) {
-                context.read<AppSettings>().updateDarkMode(val);
-              },
+          FeaturesTour(
+            controller: tourController,
+            index: 0,
+            introduce: const Text("Change Theme Between Light and Dark Mode"),
+            child: ListTile(
+              leading: const Icon(Icons.brightness_4),
+              title: const Text("Dark Mode"),
+              trailing: Switch(
+                value: isDark,
+                onChanged: (val) {
+                  context.read<AppSettings>().updateDarkMode(val);
+                },
+              ),
             ),
           ),
-          ListTile(
-            leading: const Icon(Icons.lock),
-            title: const Text("Security"),
-            trailing: Switch(
-              value: isAuth,
-              onChanged: (val) {
-                context.read<AppSettings>().updateAuthMode(val);
-              },
+          FeaturesTour(
+            controller: tourController,
+            index: 1,
+            introduce: const Text("Turn On or Off Local Authenication"),
+            child: ListTile(
+              leading: const Icon(Icons.lock),
+              title: const Text("Security"),
+              trailing: Switch(
+                value: isAuth,
+                onChanged: (val) {
+                  context.read<AppSettings>().updateAuthMode(val);
+                },
+              ),
             ),
           ),
-          ListTile(
-            leading: const Icon(Icons.storage),
-            title: const Text("Backup"),
-            onTap: () async {
-              bool temp = await backupIt();
-              var message = "";
-              if (temp) {
-                message = "Backup Complete";
-              } else {
-                message = "Backup Failed";
-              }
+          FeaturesTour(
+            controller: tourController,
+            index: 2,
+            introduce: const Text("Created A Backup for Your Password"),
+            child: ListTile(
+              leading: const Icon(Icons.storage),
+              title: const Text("Backup"),
+              onTap: () async {
+                bool temp = await backupIt();
+                var message = "";
+                if (temp) {
+                  message = "Backup Complete";
+                } else {
+                  message = "Backup Failed";
+                }
 
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(message)));
-            },
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(message)));
+              },
+            ),
           ),
-          ListTile(
-            leading: const Icon(Icons.restore),
-            title: const Text("Restore Password"),
-            onTap: () async {
-              bool temp = await restoreIt();
-              var message = "";
-              if (temp) {
-                message = "Data Restore Complete";
-              } else {
-                message = "Data Restore Failed";
-              }
-              context.read<PasswordHelper>().getPasswords();
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(message)));
-            },
+          FeaturesTour(
+            controller: tourController,
+            index: 3,
+            introduce: const Text("Restore Data using Backup File"),
+            child: ListTile(
+              leading: const Icon(Icons.restore),
+              title: const Text("Restore Password"),
+              onTap: () async {
+                bool temp = await restoreIt();
+                var message = "";
+                if (temp) {
+                  message = "Data Restore Complete";
+                } else {
+                  message = "Data Restore Failed";
+                }
+                context.read<PasswordHelper>().getPasswords();
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(message)));
+              },
+            ),
           ),
         ],
       ),
