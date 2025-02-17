689af01784db3680bf36ee8e2b7843fc91f1bf30
archp4 <archpatel4@gmail.com>
2025-02-17 15:46:06 -0500

    Safekeeper

---
+++ lib/models/safekeeper.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+class SafeKeeper {
+  // Encrypt the text with your algorithm
+  static String encrypt(String text) {
+    return text;
+  }
+
+  // Decrypt the text with your algorithm
+  static String decrypt(String text) {
+    return text;
+  }
+}
